b7062fdf1c46956f6cb954034a61df37bc4e1d4c
Saleh Yasin <salehmax2004@gmail.com>
2022-06-25 17:10:48 +0300

    Beta v1.1, added variable ball speed and tweaked wall responsiveness

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,12 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Ball : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
     public Vector3 startPosition;
+
+    //random float generator
+    static float RandFloat(float min, float max)
+    {
+        System.Random random = new System.Random();
+        double val = (random.NextDouble() * (max - min) + min);
+        return (float)val;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +43,10 @@ public class Ball : MonoBehaviour
         //   col.gameObject is the racket
         //   col.transform.position is the racket's position
         //   col.collider is the racket's collider
+
+        //randomize speed before collision
+        speed = RandFloat(5, 9);
+
 
         // Hit the left Racket?
         if (col.gameObject.name == "Player1")
